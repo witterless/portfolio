0762cf710ee9ffbb566ce9639b133a6d35ddd9e8
Louise Spiers <louisespiers123@hotmail.com>
2020-08-21 17:32:57 +0100

    Updates

---
+++ precache-manifest.fd28161dc154ff3103171b759db74fb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5906a8402c9760cc69da4177c689b9",
+    "url": "/github-pages-react-app/index.html"
+  },
+  {
+    "revision": "afed5959e7e8c56eb850",
+    "url": "/github-pages-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/github-pages-react-app/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-react-app/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afed5959e7e8c56eb850",
+    "url": "/github-pages-react-app/static/js/main.a0ab0405.chunk.js"
+  },
+  {
+    "revision": "19232e9968cf7f11e851",
+    "url": "/github-pages-react-app/static/js/runtime-main.06981ed6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
